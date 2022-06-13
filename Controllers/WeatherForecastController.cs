63409f2a86462dab7e3d2eb2be9f24deb8d55eaf
Oviya <ovi.nerd@gmail.com>
2022-06-14 01:19:47 +0530

    SameTenEntries

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace MyWebApiAzure.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering","Haze" "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering","Haze","Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
